182b93050111c2ee0ae837b71f645a469dbdfbcc
Henry <henrysungyun@gmail.com>
2023-01-19 13:54:48 -0800

    commit tree 75671337c8a7047e538de9735b1e4a067e36469a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(name, eventName) {
+    for (let t = 0; t < name.length; t++) {
+        console.log(`Thank you, ${name[t]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return name, eventName
+}
+
+writeCards (["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(positiveInteger) {
+    let p = 0
+    while (p > 0) {
+        console.log(`${positiveInteger}`);
+        p++;
+    }
+
+    return positiveInteger;
+}
